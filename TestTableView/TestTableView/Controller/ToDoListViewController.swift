a501b72eac93bdbf5af8dc85bc349067503ab687
nunump <nunuid17@gmail.com>
2018-02-15 14:24:33 +0700

    tambahan core data: save dan reload

--- TestTableView/TestTableView/Controller/ToDoListViewController.swift
+++ TestTableView/TestTableView/Controller/ToDoListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -14,41 +15,47 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
     
     var itemArray = [Item]() // isi defaultnya di ambil dari yg ada di func didload
     //let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+       print("\(dataFilePath) Ini Data File Path")
+        
+        loadItem()
         
         // isi array defaults, gak usah jg gak papa
         
-        let newItem = Item()
-        newItem.nama = "Nunu"
-        newItem.alamat = "Purbalingga"
-        itemArray.append(newItem)
+//        let newItem = Item(context: self.context)
+//        newItem.nama = "Nunu"
+//        newItem.alamat = "Purbalingga"
+//        itemArray.append(newItem)
+//
+//        let newItem1 = Item(context: self.context)
+//        newItem1.nama = "Hafid"
+//        newItem1.alamat = "BBS"
+//        itemArray.append(newItem1)
         
-        let newItem1 = Item()
-        newItem1.nama = "Hafid"
-        newItem1.alamat = "BBS"
-        itemArray.append(newItem1)
-        
-        let newItem2 = Item()
-        newItem2.nama = "Denis"
-        newItem2.alamat = "Jogja"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.nama = "Tri"
-        newItem3.alamat = "Sumatra"
-        itemArray.append(newItem3)
-        
-        let newItem4 = Item()
-        newItem4.nama = "Wilian"
-        newItem4.alamat = "Magelang"
-        itemArray.append(newItem4)
+//        let newItem2 = Item()
+//        newItem2.nama = "Denis"
+//        newItem2.alamat = "Jogja"
+//        itemArray.append(newItem2)
+//
+//        let newItem3 = Item()
+//        newItem3.nama = "Tri"
+//        newItem3.alamat = "Sumatra"
+//        itemArray.append(newItem3)
+//
+//        let newItem4 = Item()
+//        newItem4.nama = "Wilian"
+//        newItem4.alamat = "Magelang"
+//        itemArray.append(newItem4)
         
         tblView.delegate = self
         tblView.dataSource = self
+        tblView.separatorColor = UIColor.clear
         
        
         
@@ -126,7 +133,7 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
             
             
             
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.nama = textField.text!
             self.itemArray.append(newItem)
             
@@ -151,8 +158,23 @@ class ToDoListViewController: UIViewController, UITableViewDelegate, UITableView
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
-        
+    }
+    
+    func saveItem(){
+        do {
+            try context.save()
+        } catch {
+            print("Error save data")
+        }
+    }
+    
+    func loadItem(){
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error Load Data from Database \(error)")
+        }
     }
     
 }
--- TestTableView/TestTableView/Supporting File/AppDelegate.swift
+++ TestTableView/TestTableView/Supporting File/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // ini yg paling pertama di jalankan sebelum didload
-        print("Did finish launching with option")
+
         print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
         return true
     }
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        print("App masuk ke dalam background") // ini ketika smartphone masuk ke home (background)
+
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -40,7 +40,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-      print("App will terminate")
+ 
+        self.saveContext()
+    }
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 
 
